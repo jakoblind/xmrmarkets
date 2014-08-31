46a658d7300735be17b456de2636869ce09261e7
Jakob Lind <karl.jakob.lind@gmail.com>
2014-08-31 21:55:46 +0200

    added simple web socket in backend
    and started in fronted

--- src/xmrmarkets/render.cljs
+++ src/xmrmarkets/render.cljs
@@ -3,6 +3,10 @@
 	    [quiescent.dom :as d]))
 
 (q/defcomponent Ticker []
-  (apply d/div {:className "lolz"}))
+  (apply d/div {:className "lolz"} "lolz"))
 
 (q/render (Ticker) (.getElementById js/document "main"))
+
+(def ws (js/WebSocket. "ws://localhost:8080/w"))
+
+(.log js/console (.-getReady ws))
--- src/xmrmarkets/web.clj
+++ src/xmrmarkets/web.clj
@@ -1,5 +1,5 @@
 (ns xmrmarkets.web
-  (:require [org.httpkit.server :refer [run-server]])
+  (:require [org.httpkit.server :as h])
   (:require [compojure.core :refer [defroutes GET]])
   (:require [compojure.route :refer [resources]]))
 
@@ -8,10 +8,18 @@
    :headers {"Content-Type" "text/html"}
    :body    "hello HTTP!"})
 
+(defn handler [request]
+  (h/with-channel request channel
+    (println channel " connected")
+    (h/on-close channel (fn [status] (println "channel closed: " status)))
+    (h/on-receive channel (fn [data] ;; echo it back
+			  (h/send! channel data)))))
+
 (defroutes app
   (resources "/")
-  (GET "/" [] index))
+  (GET "/" [] index)
+  (GET "/w" [] handler))
 
 (defn -main [& args]
-  (run-server app {:port 8080})
+  (h/run-server app {:port 8080})
   (println "server started"))
