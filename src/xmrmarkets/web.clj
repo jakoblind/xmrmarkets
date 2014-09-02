4e61b5576a402346aa35d8e84598401afe4b610c
Jakob Lind <karl.jakob.lind@gmail.com>
2014-09-02 14:04:51 +0200

    update ticker info every 5 sec

--- src/xmrmarkets/web.clj
+++ src/xmrmarkets/web.clj
@@ -12,25 +12,36 @@
 	    [clojure.core.async :refer [<! >! put! close! go go-loop timeout]]))
 
 (defn get-xmr-ticker []
+  (log/info "called get xmr ticker webservice PROD")
   (http/get "https://poloniex.com/public?command=returnTicker"
 	    (fn [{:keys [status headers body error]}]
 	      (get (json/read-str body) "BTC_XMR"))))
 
-(defn get-xmr-ticker-test [] {:last 0.0042, :lowestAsk 0.00420393, :highestBid 0.0042, :percentChange -0.07950451, :baseVolume 390.0300689, :quoteVolume 87916.16362306, :isFrozen 99})
+(defn get-xmr-ticker-test []
+  (log/info "called get xmr ticker webservice TEST")
+  {:last 0.0042, :lowestAsk 0.00420393, :highestBid 0.0042, :percentChange -0.07950451, :baseVolume 390.0300689, :quoteVolume 87916.16362306, :isFrozen 99})
+
+(def latest-xmr-ticker (atom (get-xmr-ticker-test)))
+
+(defn update-ticker [] (future
+		     (while true
+		       (reset! latest-xmr-ticker (get-xmr-ticker-test))
+		       (Thread/sleep 5000))
+		     ))
+
+(defn ws-handler [{:keys [ws-channel] :as req}]
+  (println "Opened connection from" (:remote-addr req))
+  (let [t @latest-xmr-ticker]
+    (log/info t)
+    (go-loop []
+      (<! (timeout 1000))
+      (>! ws-channel t)
+      (recur))))
 
 (defn index [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body    "hello HTTP!"})
-
-
-(defn ws-handler [{:keys [ws-channel] :as req}]
-  (println "Opened connection from" (:remote-addr req))
-  (let [t (get-xmr-ticker-test)]
-    (go-loop []
-      (<! (timeout 1000))
-      (>! ws-channel t)
-      (recur))))
 
 (defroutes routes
   (resources "/")
@@ -49,4 +60,5 @@
 	  (System/exit 0))
     (let [handler (if in-dev? (reload/wrap-reload (site #'routes)) (site routes))]
       (run-server handler {:port 8080})
+      (update-ticker)
       (log/info "server started"))))
