2f919860fe420e4077ffced64b664145709775c3
Jakob Lind <karl.jakob.lind@gmail.com>
2014-09-03 15:38:36 +0200

    add logic for showing up or down movement in hist

--- src/xmrmarkets/render.cljs
+++ src/xmrmarkets/render.cljs
@@ -10,7 +10,7 @@
   (apply d/div {} t))
 
 (q/defcomponent HistoryItem [h]
-  (apply d/div {} (h "amount") ", " (h "date") ", " (h "rate") ", up or down"))
+  (apply d/div {} (h "amount") "XMR, " (h "date") ", " (h "rate") ", " (h "up-down")))
 
 (.add (.-tz js/moment) "Etc/UTC|UTC|0|0|")
 
@@ -19,14 +19,21 @@
          (defn sort-by-date [m]
            (sort-by #(vec (map % "date")) m))
          (defn date-human-readable [m]
-           (map (fn [i] (update-in i ["date"] (fn [a] (.fromNow (.tz js/moment a "YYYY-MM-DD hh:mm:ss" "Etc/UTC"))))) m))
-         (let [sort (sort-by-date (date-human-readable h))]
+           (map (fn [i]
+                  (update-in i ["date"]
+                             (fn [a]
+                               (.fromNow
+                                (.tz js/moment a "YYYY-MM-DD hh:mm:ss" "Etc/UTC"))))) m))
+         (defn add-down-up [v i]
+           (let [up-down (if (= (first v) null) "" (if (>= (i "rate") ((first v) "rate")) "up" "down"))]
+             (cons (assoc i "up-down" up-down) v)))
+         (let [sort (reduce add-down-up [] (reverse (sort-by-date (date-human-readable h))))]
            (map HistoryItem sort))))
 
 (q/defcomponent PriceInfo [d]
   (d/div {}
          (Ticker ((d "ticker") "last"))
-         (History (d "history"))))
+         (History (take 5 (d "history")))))
 
 (enable-console-print!)
 
