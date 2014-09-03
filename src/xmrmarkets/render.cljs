1b2031bd4617ab86529ad72070944ff0fef0206a
Jakob Lind <karl.jakob.lind@gmail.com>
2014-09-03 14:12:59 +0200

    lot of stuff
    - get trade history from additional ws
    - send it to render.js
    - sort history by date
    - print date human readable

--- src/xmrmarkets/render.cljs
+++ src/xmrmarkets/render.cljs
@@ -1,21 +1,37 @@
 (ns xmrmarkets.render
 (:require [chord.client :refer [ws-ch]]
-	  [cljs.core.async :refer [chan <! >! put! close! timeout]]
-	  [quiescent :as q :include-macros true]
-	  [quiescent.dom :as d])
+          [cljs.core.async :refer [chan <! >! put! close! timeout]]
+          [quiescent :as q :include-macros true]
+          [quiescent.dom :as d])
 (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
 (q/defcomponent Ticker [t]
   (prn t)
-  (apply d/div {:className "lolz"} (get t "last")))
+  (apply d/div {} t))
 
+(q/defcomponent HistoryItem [h]
+  (apply d/div {} (h "amount") ", " (h "date") ", " (h "rate") ", up or down"))
+
+(q/defcomponent History [h]
+  (apply d/div {}
+         (defn sort-by-date [m]
+           (sort-by #(vec (map % "date")) m))
+         (defn date-human-readable [m]
+           (map (fn [i] (update-in i ["date"] (fn [a] (.fromNow (js/moment. a "YYYY-MM-DD hh:mm:ss"))))) m))
+         (let [sort (sort-by-date (date-human-readable h))]
+           (map HistoryItem sort))))
+
+(q/defcomponent PriceInfo [d]
+  (d/div {}
+         (Ticker ((d "ticker") "last"))
+         (History (d "history"))))
 
 (enable-console-print!)
 
 (go
   (let [server-ch (<! (ws-ch "ws://localhost:8080/ws"{:format :edn}))
-	container (.getElementById js/document "main")]
+        container (.getElementById js/document "main")]
     (go-loop []
-      (when-let [t (:message (<! server-ch))]
-	(q/render (Ticker t) container)
-	(recur)))))
+      (when-let [d (:message (<! server-ch))]
+        (q/render (PriceInfo d) container)
+        (recur)))))
--- src/xmrmarkets/web.clj
+++ src/xmrmarkets/web.clj
@@ -1,41 +1,51 @@
 (ns xmrmarkets.web
   (:require [org.httpkit.server :refer [run-server]]
-	    [org.httpkit.client :as http]
-	    [ring.middleware.reload :as reload]
-	    [compojure.handler :refer [site]]
-	    [compojure.core :refer [defroutes GET]]
-	    [compojure.route :refer [resources]]
-	    [chord.http-kit :refer [wrap-websocket-handler]]
-	    [clojure.data.json :as json]
-	    [clojure.tools.cli :refer [cli]]
-	    [clojure.tools.logging :as log]
-	    [clojure.core.async :refer [<! >! put! close! go go-loop timeout]]))
+            [org.httpkit.client :as http]
+            [ring.middleware.reload :as reload]
+            [compojure.handler :refer [site]]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [resources]]
+            [chord.http-kit :refer [wrap-websocket-handler]]
+            [clojure.data.json :as json]
+            [clojure.tools.cli :refer [cli]]
+            [clojure.tools.logging :as log]
+            [clojure.core.async :refer [<! >! put! close! go go-loop timeout]]))
 
 (defn get-xmr-ticker []
   (log/info "called get xmr ticker webservice PROD")
   (http/get "https://poloniex.com/public?command=returnTicker"
-	    (fn [{:keys [status headers body error]}]
-	      (get (json/read-str body) "BTC_XMR"))))
+            (fn [{:keys [status headers body error]}]
+              (get (json/read-str body) "BTC_XMR"))))
 
-(defn get-xmr-ticker-test []
-  (log/info "called get xmr ticker webservice TEST")
-  {:last 0.0042, :lowestAsk 0.00420393, :highestBid 0.0042, :percentChange -0.07950451, :baseVolume 390.0300689, :quoteVolume 87916.16362306, :isFrozen 99})
+(defn get-xmr-trade-history []
+  (log/info "called get xmr ticker webservice PROD")
+  (http/get "https://poloniex.com/public?command=returnTradeHistory&currencyPair=BTC_XMR"
+            (fn [{:keys [status headers body error]}]
+              (json/read-str body))))
 
-(def latest-xmr-ticker (atom (get-xmr-ticker-test)))
+(defn get-xmr-all []  {"ticker" @(get-xmr-ticker) "history" @(get-xmr-trade-history)})
+
+(println @(get-xmr-trade-history))
+
+(defn get-xmr-all-test []
+  (log/info "called get xmr all webservice TEST")
+   {"last" 0.00410137, "lowestAsk" 0.00412999, "highestBid" 0.00410137, "percentChange" -0.01171807, "baseVolume" 251.06992921, "quoteVolume" 61852.85822063, "isFrozen" 0})
+
+(def latest-xmr-ticker (atom (get-xmr-all)))
 
 (defn update-ticker [] (future
-		     (while true
-		       (reset! latest-xmr-ticker (get-xmr-ticker-test))
-		       (Thread/sleep 5000))
-		     ))
+                         (while true
+                           (reset! latest-xmr-ticker (get-xmr-all))
+                           (Thread/sleep 60000))
+                         ))
 
 (defn ws-handler [{:keys [ws-channel] :as req}]
   (println "Opened connection from" (:remote-addr req))
   (let [t @latest-xmr-ticker]
     (log/info t)
     (go-loop []
-      (<! (timeout 1000))
       (>! ws-channel t)
+      (<! (timeout 1000))
       (recur))))
 
 (defn index [req]
@@ -47,17 +57,17 @@
   (resources "/")
   (GET "/" [] index)
   (GET "/ws" [] (-> ws-handler
-		    (wrap-websocket-handler {:format :edn}))))
+                    (wrap-websocket-handler {:format :edn}))))
 
 (defn -main [& args]
   (let [[options args banner]
-	(cli args
-	     ["-h" "--help" "Show Help" :default false :flag true]
-	     ["-d" "--development" "Run server in development mode" :default false :flag true])]
+        (cli args
+             ["-h" "--help" "Show Help" :default false :flag true]
+             ["-d" "--development" "Run server in development mode" :default false :flag true])]
     (defonce in-dev? (:development options))
     (when (:help options)
-	  (println banner)
-	  (System/exit 0))
+          (println banner)
+          (System/exit 0))
     (let [handler (if in-dev? (reload/wrap-reload (site #'routes)) (site routes))]
       (run-server handler {:port 8080})
       (update-ticker)
