52ddaa708eed052a13b1abcb193de689e5923e9b
Jakob Lind <karl.jakob.lind@gmail.com>
2014-09-10 19:17:58 +0000

    added candlestick chart with d3 to main page

--- resources/public/candlestick.js
+++ resources/public/candlestick.js
@@ -1,4 +1,5 @@
 //http://bl.ocks.org/abeppu/1074045
+window.XMR = window.XMR || {};
 (function(){
     "use strict";
 
@@ -11,80 +12,80 @@
     function max(a, b){ return a > b ? a : b; }
 
     function buildChart(data){
-	var margin = 50;
+        var margin = 50;
 
-	var chart = d3.select("#chart")
-	    .append("svg:svg")
-	    .attr("class", "chart")
-	    .attr("width", width)
-	    .attr("height", height);
-	var y = d3.scale.linear()
-	    .domain([d3.min(data.map(function(x) {return x["low"];})), d3.max(data.map(function(x){return x["high"];}))])
-	    .range([height-margin, margin]);
-	var x = d3.scale.linear()
-	    .domain([d3.min(data.map(function(d){return d.date})),
-		     d3.max(data.map(function(d){return d.date}))])
-	    .range([margin,width-margin]);
+        var chart = d3.select("#chart")
+            .append("svg:svg")
+            .attr("class", "chart")
+            .attr("width", width)
+            .attr("height", height);
+        var y = d3.scale.linear()
+            .domain([d3.min(data.map(function(x) {return x["low"];})), d3.max(data.map(function(x){return x["high"];}))])
+            .range([height-margin, margin]);
+        var x = d3.scale.linear()
+            .domain([d3.min(data.map(function(d){return d.date})),
+                     d3.max(data.map(function(d){return d.date}))])
+            .range([margin,width-margin]);
 
-	chart.selectAll("line.x")
-	    .data(x.ticks(10))
-	    .enter().append("svg:line")
-	    .attr("class", "x")
-	    .attr("x1", x)
-	    .attr("x2", x)
-	    .attr("y1", margin)
-	    .attr("y2", height - margin)
-	    .attr("stroke", "#ccc");
+        chart.selectAll("line.x")
+            .data(x.ticks(10))
+            .enter().append("svg:line")
+            .attr("class", "x")
+            .attr("x1", x)
+            .attr("x2", x)
+            .attr("y1", margin)
+            .attr("y2", height - margin)
+            .attr("stroke", "#ccc");
 
-	chart.selectAll("line.y")
-	    .data(y.ticks(10))
-	    .enter().append("svg:line")
-	    .attr("class", "y")
-	    .attr("x1", margin)
-	    .attr("x2", width - margin)
-	    .attr("y1", y)
-	    .attr("y2", y)
-	    .attr("stroke", "#ccc");
+        chart.selectAll("line.y")
+            .data(y.ticks(10))
+            .enter().append("svg:line")
+            .attr("class", "y")
+            .attr("x1", margin)
+            .attr("x2", width - margin)
+            .attr("y1", y)
+            .attr("y2", y)
+            .attr("stroke", "#ccc");
 
-	chart.selectAll("text.xrule")
-	    .data(x.ticks(10))
-	    .enter().append("svg:text")
-	    .attr("class", "xrule")
-	    .attr("x", x)
-	    .attr("y", height - margin)
-	    .attr("dy", 20)
-	    .attr("text-anchor", "middle")
-	    .text(function(d){ var date = new Date(d * 1000);  return (date.getMonth() + 1)+"/"+date.getDate(); });
+        chart.selectAll("text.xrule")
+            .data(x.ticks(10))
+            .enter().append("svg:text")
+            .attr("class", "xrule")
+            .attr("x", x)
+            .attr("y", height - margin)
+            .attr("dy", 20)
+            .attr("text-anchor", "middle")
+            .text(function(d){ var date = new Date(d * 1000);  return (date.getMonth() + 1)+"/"+date.getDate(); });
 
-	chart.selectAll("text.yrule")
-	    .data(y.ticks(10))
-	    .enter().append("svg:text")
-	    .attr("class", "yrule")
-	    .attr("x", width - margin)
-	    .attr("y", y)
-	    .attr("dy", 0)
-	    .attr("dx", 20)
-	    .attr("text-anchor", "middle")
-	    .text(String);
+        chart.selectAll("text.yrule")
+            .data(y.ticks(10))
+            .enter().append("svg:text")
+            .attr("class", "yrule")
+            .attr("x", width - margin)
+            .attr("y", y)
+            .attr("dy", 0)
+            .attr("dx", 20)
+            .attr("text-anchor", "middle")
+            .text(String);
 
-	chart.selectAll("rect")
-	    .data(data)
-	    .enter().append("svg:rect")
-	    .attr("x", function(d) { return x(d.date)})
-	    .attr("y", function(d) {return y(max(d.open, d.close));})
-	    .attr("height", function(d) { return y(min(d.open, d.close))-y(max(d.open, d.close));})
-	    .attr("width", function(d) { return 0.5 * (width - 2*margin)/data.length; })
-	    .attr("fill",function(d) { return d.open > d.close ? "red" : "green" ;});
+        chart.selectAll("rect")
+            .data(data)
+            .enter().append("svg:rect")
+            .attr("x", function(d) { return x(d.date)})
+            .attr("y", function(d) {return y(max(d.open, d.close));})
+            .attr("height", function(d) { return y(min(d.open, d.close))-y(max(d.open, d.close));})
+            .attr("width", function(d) { return 0.5 * (width - 2*margin)/data.length; })
+            .attr("fill",function(d) { return d.open > d.close ? "red" : "green" ;});
 
-	chart.selectAll("line.stem")
-	    .data(data)
-	    .enter().append("svg:line")
-	    .attr("class", "stem")
-	    .attr("x1", function(d) { return x(d.date) + 0.25 * (width - 2 * margin)/ data.length;})
-	    .attr("x2", function(d) { return x(d.date) + 0.25 * (width - 2 * margin)/ data.length;})
-	    .attr("y1", function(d) { return y(d.high);})
-	    .attr("y2", function(d) { return y(d.low); })
-	    .attr("stroke", function(d){ return d.open > d.close ? "red" : "green"; })
+        chart.selectAll("line.stem")
+            .data(data)
+            .enter().append("svg:line")
+            .attr("class", "stem")
+            .attr("x1", function(d) { return x(d.date) + 0.25 * (width - 2 * margin)/ data.length;})
+            .attr("x2", function(d) { return x(d.date) + 0.25 * (width - 2 * margin)/ data.length;})
+            .attr("y1", function(d) { return y(d.high);})
+            .attr("y2", function(d) { return y(d.low); })
+            .attr("stroke", function(d){ return d.open > d.close ? "red" : "green"; })
 
     }
 
