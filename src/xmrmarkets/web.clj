e7f31e92be2202dd696a47f790919cd8374717f8
Jakob Lind <karl.jakob.lind@gmail.com>
2014-09-17 19:46:44 +0200

    text updates

--- src/xmrmarkets/web.clj
+++ src/xmrmarkets/web.clj
@@ -27,12 +27,12 @@
 (defn date-relative-string [d]
   (let [diff-sec
         (long (/ (- (c/to-long (t/to-time-zone (t/now) (t/time-zone-for-offset 0))) (c/to-long (f/parse (f/formatter "yyyy-MM-dd HH:mm:ss") d))) 1000))]
-    (cond (<= diff-sec 10) "a few seconds ago"
-          (< diff-sec 60) (str diff-sec " seconds ago")
-          (< diff-sec 120) "1 minute ago"
-          (<= diff-sec (* 60 60)) (str (long (/ diff-sec 60)) " minutes ago")
-          (< diff-sec (* 60 60 2)) "1 hour ago"
-          (<= diff-sec (* 60 60 12)) (str (long (/ diff-sec (* 60 60))) " hours ago")
+    (cond (<= diff-sec 10) "a few sec ago"
+          (< diff-sec 60) (str diff-sec " sec ago")
+          (< diff-sec 120) "1 min ago"
+          (<= diff-sec (* 60 60)) (str (long (/ diff-sec 60)) " min ago")
+          (< diff-sec (* 60 60 2)) "1 h ago"
+          (<= diff-sec (* 60 60 12)) (str (long (/ diff-sec (* 60 60))) " h ago")
           (<= diff-sec (* 60 60 24)) "yesterday"
           :else d)))
 
